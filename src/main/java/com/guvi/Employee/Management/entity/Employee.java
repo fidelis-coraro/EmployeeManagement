9920a447a054986f829aba034da8afedbd8a2535
fidelis-coraro <160701865+fidelis-coraro@users.noreply.github.com>
2024-05-25 20:37:53 +0530

    Mini project1

--- src/main/java/com/guvi/Employee/Management/entity/Employee.java
+++ src/main/java/com/guvi/Employee/Management/entity/Employee.java
@@ -1,15 +1,21 @@
 package com.guvi.Employee.Management.entity;
 
 
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import jakarta.persistence.*;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/guvi/Employee/Management/exception/ResourceNotFoundException.java
+++ src/main/java/com/guvi/Employee/Management/exception/ResourceNotFoundException.java
@@ -1,6 +1,10 @@
 package com.guvi.Employee.Management.exception;
 
+import java.io.Serial;
+
+
 public class ResourceNotFoundException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public ResourceNotFoundException(String message) {
